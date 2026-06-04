175f65f9d33c33ad8bb7a4456f0b73012505027e
OnigiriKing <oooNikita87@gmail.com>
2026-06-04 19:58:39 +0900

    svg and some links added

---
+++ src/components/NavComp/NavComp.DropMenu/DropMenu.tsx
@@ -0,0 +1,9 @@
+
+export default function DropMenu() {
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
--- src/components/NavComp/NavComp.tsx
+++ src/components/NavComp/NavComp.tsx
@@ -1,13 +1,29 @@
 import { Link } from "react-router-dom"
+import allSvg from "../../assets/svg/AllSvg"
 
 export default function NavComp() {
+
+    const dropMenu = allSvg().dropMenu;
+    const gitHub = allSvg(30).gitHub;
+
     return (
-        <div className="w-full h-16 flex items-center justify-between px-8">
-            <Link to={"/"} className="">I wish you can read this</Link>
-            <div className="flex gap-6">
-            <div>Links</div>
-            <div>Drop down menu</div>
-            </div>
+      <div className="w-full h-16 flex items-center justify-between px-20">
+        <Link to={"/"} className="text-2xl">
+          I wish you can read this
+        </Link>
+        <div className="flex gap-6 items-center">
+          <div>
+            <Link to="https://github.com/OnigiriKing" className="">
+              {gitHub}
+            </Link>
+          </div>
+          <button
+            type="button"
+            className="items-center hover:text-orange-700 ease-in-out duration-300 cursor-pointer"
+          >
+            {dropMenu}
+          </button>
         </div>
-    )
+      </div>
+    );
 }
