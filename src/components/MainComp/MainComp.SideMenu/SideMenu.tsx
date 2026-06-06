f428ad7e4002c368b09837ab709313ff0f9732c0
OnigiriKing <oooNikita87@gmail.com>
2026-06-06 11:33:28 +0900

    iphone scale change

--- src/components/MainComp/MainComp.SideMenu/SideMenu.tsx
+++ src/components/MainComp/MainComp.SideMenu/SideMenu.tsx
@@ -1,21 +1,24 @@
 export default function SideMenu() {
   return (
-    <div className="w-100 bg-white absolute right-0 h-full flex z-20">
-      <div className="device device-iphone-14-pro">
-        <div className="device-frame">
-          <div className="device-content">Hello World</div>
+    <div className="w-100 bg-white absolute right-0 top-0 h-full flex z-20 justify-center">
+        <div>
+            
         </div>
+        <div className="device device-iphone-14-pro">
+          <div className="device-frame">
+            <div className="device-content">Hello World</div>
+          </div>
 
-        <div className="device-stripe"></div>
+          <div className="device-stripe"></div>
 
-        <div className="device-header"></div>
+          <div className="device-header"></div>
 
-        <div className="device-sensors"></div>
+          <div className="device-sensors"></div>
 
-        <div className="device-btns"></div>
+          <div className="device-btns"></div>
 
-        <div className="device-power"></div>
-      </div>
+          <div className="device-power"></div>
+        </div>
     </div>
   );
 }
