a7cf70139646a1c1865722a388151c3dc43840f2
OnigiriKing <oooNikita87@gmail.com>
2026-07-20 10:32:17 +0900

    server update, scripts added

---
+++ server/src/server.js
@@ -0,0 +1,22 @@
+// server
+const expresss = require("express");
+// security
+const cors = require("cors");
+// access to .env
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = expresss();
+const port = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/health", (req, res) => {
+  res.json({ status: "ok" });
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+})
